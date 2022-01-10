e448429c5f3a7c8f8dbdd6d3b33c34c5ecdd1a13
Eli Solomon <ESolomonDev@gmail.com>
2022-01-10 18:48:14 -0500

    added playground python

---
+++ Python_practice.py
@@ -0,0 +1,28 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+#for county, voters in counties_dict.items():
+#    print(f"{county} county has {voters} registered voters.")
+
+#for county in counties_dict.keys():
+#    print(county)
+
+#for voters in counties_dict.values():
+#    print(voters)
+
+#for county in counties_dict:
+#    print(counties_dict[county])
+
+#for county, voters in counties_dict.items():
+#    print(str(county)+" county has "+str(voters)+" registered voters")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+#for county, voters in voting_data.items():
+#    print(county, voters)
+#    print(f"{county} county has {voting_data.get(county)} registered voters.")
+
+#for county_dict in voting_data:
+#    for value in county_dict.values():
+#        print(value)
